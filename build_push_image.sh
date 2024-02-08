99daca0d98f21aea556a5532b1888318b6499ad4
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2024-02-08 22:26:10 +0700

    push docker images

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,23 +1,17 @@
 Copy code
 #!/bin/bash
-
-# Set environment variables
-DOCKER_USERNAME="farhan18"
-DOCKER_PASSWORD="2024dapatgelarskom"
-
-
 # Variabel untuk nama image yang akan dibuat
 IMAGE_NAME="item-app"
 
 # Versi dari image
-IMAGE_VERSION="1.0.0"
+IMAGE_VERSION="v1"
 
 # Jalankan perintah untuk membangun Docker image
 docker build -t $IMAGE_NAME:$IMAGE_VERSION .
 
 # Jalankan perintah untuk login ke Docker Hub
 echo "Logging in to Docker Hub..."
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "./bin/dockerpass.txt" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Tag image dengan nama pengguna Docker Hub
 docker tag $IMAGE_NAME:$IMAGE_VERSION $DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_VERSION
