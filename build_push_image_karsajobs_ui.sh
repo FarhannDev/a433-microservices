fbe2e9639847e800874e2680fc87c3035b1580ac
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2024-02-20 19:19:52 +0700

    Add script build docker image

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set environment variables
+GITHUB_USERNAME="farhanndev"
+GITHUB_TOKEN="$CR_PAT"
+
+IMAGE_NAME="karsajobs-ui"
+IMAGE_VERSION="latest"
+
+# Build Docker image for frontend
+echo "Build Docker image for frontend..."
+docker build -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$IMAGE_VERSION .
+
+# Login to GitHub Packages
+echo "Logging in to GitHub Packages..."
+docker login ghcr.io -u $GITHUB_USERNAME -p $GITHUB_TOKEN
+
+# Push image to GitHub Packages
+echo "Pushing image to GitHub Packages..."
+docker push ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$IMAGE_VERSION
